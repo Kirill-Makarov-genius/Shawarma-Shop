34662000b82bee19fa18ccf7c13cec38385b5dd0
Kirill-Makarov-genius <kiryusha.makarov.04@mail.ru>
2021-11-08 18:28:56 +0300

    First Configuratio

---
+++ src/main/java/com/makarov/shawarmaCloudShop/ShawarmaCloudShopApplication.java
@@ -0,0 +1,13 @@
+package com.makarov.shawarmaCloudShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShawarmaCloudShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShawarmaCloudShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makarov/shawarmaCloudShop/ShawarmaCloudShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makarov.shawarmaCloudShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShawarmaCloudShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
